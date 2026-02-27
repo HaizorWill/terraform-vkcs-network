752e4f18d560845d60731ff218615c39264ca2fd
HaizorWill <thelines6@gmail.com>
2026-02-27 22:47:52 +0300

    Set to track locals.tf

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  private_dns_domain = endswith(var.private_dns_domain, ".") ? var.private_dns_domain : format("${var.private_dns_domain}%s", ".")
+  subnets = var.subnets != null ? { for id, subnet in var.subnets :
+    coalesce(subnet.resource_key, subnet.name, format("subnet-%s", id)) => merge(subnet, {
+      # no_gateway         = subnet.gateway_ip != null ? false : subnet.no_gateway
+      enable_private_dns = subnet.dns_nameservers != null ? false : subnet.enable_private_dns
+    })
+  } : {}
+}
